b4dcc8c235c276c77868a3274f2e29a17c4de2fa
Leandro <leandroferreira@cjr.org.br>
2015-03-24 13:10:25 -0300

    Primeiro commit do Projeto

---
+++ algumacois.c
@@ -0,0 +1,8 @@
+
+
+1)	Nivel Operacional da organização
+2)	Nível de Conhecimento da Organização
+3)	Não,O sistema de nível gerencial ele é alimentado por relatórios do sistema de nível operacional, processando e resumindo todos os relatórios do sistema operacional, dando informações de fácil entendimento num nível maior de abstração. As respostas estão processadas e não são acessíveis diretamente.
+4)	Não sei
+5)	São sistemas que ajudam a prever cenários de no mínimo 5 anos, processando e apresentado indícios e previsões de informação.
+6)	Se é Customizável para adaptar melhor a minha empresa, se tenho suporte técnico da empresa para o sistema, o preço da ferramenta, se a manutenção é possível ou cara.
